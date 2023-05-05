bf5330e34c0f7d5d60f547fe90ec2adb094e1876
Rafael8000 <rafael.pomari@etec.sp.gov.br>
2023-05-05 15:14:39 +0000

    luvadepedreiroroubado

---
+++ app/src/main/java/startspring/Receba.java
@@ -0,0 +1,12 @@
+package startspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Receba {
+    
+    public static void main(String[] args){
+        SpringApplication.run(Receba.class, args);
+    }
+}
